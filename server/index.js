804dff26a1d4bd294d4b665ae0bfa49cab9348ad
lordkendri <76025241+lordkendri@users.noreply.github.com>
2022-03-23 18:00:32 -0400

    Twilio added

--- server/index.js
+++ server/index.js
@@ -2,15 +2,45 @@ const express = require('express');
 const cors = require('cors');
 
 const authRoutes = require("./routes/auth.js");
+const twilio = require('twilio');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 require('dotenv').config();
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_ACCOUNT_TOKEN;
+const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
+const twilioClient = require('twilio')(accountSid, authToken);
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
+
+app.post('/', (req, res) => {
+    const { message, user: sender, type, members } = req.body;
+
+    if(type === 'message.new'){
+        members
+            ,filters((member) => member.user_id !== sender.id)
+            .forEach(({ user }) => {
+                if(!user.online){
+                    twilioClient.messages.create({
+                        body: `You have a new message from ${message.user.fullName} - ${message.text}`,
+                        messagingServiceSid: messagingServiceSid,
+                        to: user.phoneNumber
+                    })
+                        .then(() => console.log('message sent!'))
+                        .catch((err) => console.log(err));
+                }
+            });
+
+            return res.status(200).send('Message sent!');
+    }
+
+    return res.status(200).setDefaultEncoding('Not a new Message request');
+});
 
 app.get('/', (req, res) => {
     res.send('Hellow, World!');
